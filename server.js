0c3e835286107e31d4e7365d204263ac24a13137
zzyo <1977795940@qq.com>
2019-11-20 22:25:59 +0800

    简易的server

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url 
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log('ff说：得到 HTTP 路径\n' + path)
+  console.log('ff说：查询字符串为\n' + query)
+  console.log('ff说：不含查询字符串的路径为\n' + pathNoQuery)
+
+
+
+
+
+
+
+
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
